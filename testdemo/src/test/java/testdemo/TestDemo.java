0c25dbd389290776fe841df5cfc6260765a92df3
emon <emonrpi@gmail.com>
2021-11-14 21:52:35 -0800

    first repoitory

---
+++ testdemo/src/test/java/testdemo/TestDemo.java
@@ -0,0 +1,14 @@
+package testdemo;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=12;
+		int result=x+y;
+		
+		System.out.println("the adding result is :"+result);
+
+	}
+
+}
